5ed89059216a696ace4aa76fbb39fb7535881828
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-01-28 22:26:07 +0100

    Register commands

--- src/main/java/dev/warriorrr/quickshopremover/QuickShopRemover.java
+++ src/main/java/dev/warriorrr/quickshopremover/QuickShopRemover.java
@@ -2,12 +2,17 @@ package dev.warriorrr.quickshopremover;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dev.warriorrr.quickshopremover.command.ClearWildernessShopsCommand;
+import dev.warriorrr.quickshopremover.command.RemoveCommand;
+
 public class QuickShopRemover extends JavaPlugin {
     public static String prefix = "[QuickShopRemover] ";
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new TownyListener(this), this);
+        getCommand("removeshop").setExecutor(new RemoveCommand());
+        getCommand("clearwildernessshops").setExecutor(new ClearWildernessShopsCommand());
     }
 
     @Override
