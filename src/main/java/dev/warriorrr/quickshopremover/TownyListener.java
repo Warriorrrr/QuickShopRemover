4820918052f24dc2a953ef9c51481722b8a338fb
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-01-28 22:26:13 +0100

    Update TownyListener.java

--- src/main/java/dev/warriorrr/quickshopremover/TownyListener.java
+++ src/main/java/dev/warriorrr/quickshopremover/TownyListener.java
@@ -2,12 +2,8 @@ package dev.warriorrr.quickshopremover;
 
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.List;
 
 import com.palmergames.bukkit.towny.event.town.TownUnclaimEvent;
-import com.palmergames.bukkit.towny.TownyUniverse;
-import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.event.PreNewDayEvent;
 
 import org.bukkit.Chunk;
 import org.bukkit.Location;
@@ -21,8 +17,8 @@ public class TownyListener implements Listener {
     @SuppressWarnings("unused")
     private final QuickShopRemover plugin;
 
-    public TownyListener(QuickShopRemover testPlugin) {
-        plugin = testPlugin;
+    public TownyListener(QuickShopRemover quickShopRemover) {
+        plugin = quickShopRemover;
     }
 
     @EventHandler
@@ -32,23 +28,5 @@ public class TownyListener implements Listener {
         for (Entry<Location, Shop> shop : shopMap.entrySet()) {
             shop.getValue().delete();
         }
-    }
-
-    @EventHandler
-    public void onNewDay(PreNewDayEvent event) {
-        int removedShops = 0;
-        List<Shop> shops = QuickShopAPI.getShopAPI().getAllShops();
-        TownyUniverse towny = TownyUniverse.getInstance();
-        for (Shop shop : shops) {
-            if (TownyAPI.getInstance().isWilderness(shop.getLocation())) {
-                shop.delete();
-                removedShops += 1;
-            }
-            else if (towny.getResident(shop.getOwner()) == null) {
-                shop.delete();
-                removedShops += 1;
-            }
-        }
-        System.out.println(QuickShopRemover.prefix + "Removed " + removedShops + " shop(s) during latest Towny NewDay.");
     }
 }
